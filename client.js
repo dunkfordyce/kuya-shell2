2b0d558edf20c85a89ee12f30ff288b7723e8aaf
Dunk Fordyce <dunkfordyce@gmail.com>
2011-06-22 08:50:05 +0100

    fiddling to get it running in the browser

---
+++ client.js
@@ -0,0 +1,15 @@
+var context = require('./context'),
+    inflater = require('./inflate').default_inflater,
+    $ = require('jquery-browserify');
+
+$.ajaxSetup({dataType: 'json'});
+
+$.ajax('/context/', {
+        type: 'POST'
+    })
+    .success(function(ret) {
+        window.context = inflater.inflate(ret);
+    })
+;
+
+console.log('systems are go');
